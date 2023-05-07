707b002d5385f4d27875fd28577a7533020de7b6
Matheus <matheussss6613@gmail.com>
2023-05-07 02:27:09 -0300

    fim

--- src/view/Main.java
+++ src/view/Main.java
@@ -4,7 +4,7 @@ import matheus.ListaInt.ListaInt;
 
 public class Main {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws Exception
 	{
 		int[] vetA = {3, 5, 8, 12, 9, 7, 16};
 		int[] vetB = {9, 6, 2, 3, 7};
@@ -21,98 +21,71 @@ public class Main {
 		A.addFirst(vetA[0]);
 		B.addFirst(vetB[0]);
 		
-		try {
-			for(int i = 1; i < tamA; i++)
-			{
-				A.addLast(vetA[i]);
-			}
-			
-			for(int i = 1; i < tamB; i++)
-			{
-				B.addLast(vetB[i]);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
+
+		for(int i = 1; i < tamA; i++)
+		{
+			A.addLast(vetA[i]);
 		}
 		
+		for(int i = 1; i < tamB; i++)
+		{
+			B.addLast(vetB[i]);
+		}
 		
 		for (int i = 0; i < tamA; i++)
 		{
-			try {
-				for(int j = 0; j < tamB; j++)
+			for(int j = 0; j < tamB; j++)
+			{
+				if(A.get(i) == B.get(j))
 				{
-					if(A.get(i) == B.get(j))
-					{
-						I.addFirst(A.get(i));							
-					}
+					I.addFirst(A.get(i));							
 				}
-			} catch (Exception e) {
-				e.printStackTrace();
 			}
 		}
 		
 		for (int i = 0; i < (A.size() > B.size() ? A.size() : B.size()); i++)
 		{
-			try {
-				if(i < A.size())
-				{
-					U.addFirst(A.get(i));					
-				}
-				if (i < B.size())
-				{
-					U.addFirst(B.get(i));
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
+			if(i < A.size())
+			{
+				U.addFirst(A.get(i));					
+			}
+			if (i < B.size())
+			{
+				U.addFirst(B.get(i));
 			}
 		}
 		
 		for (int i = 0; i < U.size(); i++)
 		{
-			int cont = 0;
+		int cont = 0;
 			for (int j = 0; j < U.size(); j++)
 			{
-				try {
-				   if (U.get(i) == U.get(j))
-				   {
-					   if (cont != 0)
-					   {
-						   U.remove(j);
-					   }
-					   cont++;
-				   }
-			   } catch (Exception e) {
-				   e.printStackTrace();
-			   }
-		   }
+				if (U.get(i) == U.get(j))
+				{
+					if (cont != 0)
+					{
+						U.remove(j);
+					}
+					cont++;
+				}
+			}
 		}
-		
 		
 		System.out.print("Interseccao\n");
 		
 		while(!I.isEmpty())
-		{
-			try {
-				System.out.println(I.get(0));
-				I.removeFirst();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+		{		
+			System.out.println(I.get(0));
+			I.removeFirst();
 		}
 		
 		System.out.println("\nUniao");
 		
 		while(!U.isEmpty())
 		{
-			try {
-				System.out.println(U.get(0));
-				U.removeFirst();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+			System.out.println(U.get(0));
+			U.removeFirst();
 		}
-
-		
 	}
 
 }
